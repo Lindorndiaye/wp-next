b95a222449091cab8fd209d398ac6664318f16c0
Lindorndiaye <lindoramadou@gmail.com>
2026-02-08 03:08:05 +0100

    restor acc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -159,14 +159,7 @@ export default async function RootLayout({
           </RevealFx>
           <Flex fillWidth minHeight="16" s={{ hide: true }} />
           <Header />
-          <Flex
-            as="main"
-            zIndex={0}
-            fillWidth
-            padding="l"
-            horizontal="center"
-            flex={1}
-          >
+          <Flex zIndex={0} fillWidth padding="l" horizontal="center" flex={1}>
             <Flex horizontal="center" fillWidth minHeight="0">
               <RouteGuard>{children}</RouteGuard>
             </Flex>
